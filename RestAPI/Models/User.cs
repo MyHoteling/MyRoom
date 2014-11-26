d7b761c90b20f8932b6c694feda8626bd2c2dcfa
Javier Trejo <jtrejo@itserviciosglobales.com.ar>
2014-11-26 16:17:00 -0300

    API para la gestión de usuarios (Tabsheet "User Data")

---
+++ RestAPI/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestAPI.Models
+{
+    [Table("BACKEND_USER")]
+    public class User
+    {
+        [Key]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [Column("Name", TypeName="varchar")]
+        [StringLength(100)]
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        [Column("Surname", TypeName="varchar")]
+        [StringLength(100)]
+        public string Surname
+        {
+            get;
+            set;
+        }
+
+        [Required(ErrorMessage="The email is required")]
+        [Column("E-mail", TypeName="varchar")]
+        [Index(IsUnique=true)]
+        [StringLength(100)]
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        [Column("Password", TypeName="varchar")]
+        [StringLength(50)]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Required(ErrorMessage="The user status is required")]
+        [Column("Active", TypeName="bit")]
+        public bool Active
+        {
+            get;
+            set;
+        }
+    }
+}
--- RestAPI/Users/UserController.cs
+++ RestAPI/Users/UserController.cs
@@ -1,39 +1,160 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
+using System.Security.Cryptography;
+using RestAPI.Models;
+using System.Text;
 
 namespace RestAPI.Users
 {
     public class UserController : ApiController
     {
+        private RestAPIContext db = new RestAPIContext();
+
         // GET: api/User
-        public IEnumerable<string> Get()
+        public IQueryable<User> GetUsers()
         {
-            return new string[] { "value1", "value2" };
+            return db.Users;
         }
 
         // GET: api/User/5
-        public string Get(int id)
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
         {
-            return "value";
-        }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
 
-        // POST: api/User
-        public void Post([FromBody]string value)
-        {
+            return Ok(user);
         }
 
         // PUT: api/User/5
-        public void Put(int id, [FromBody]string value)
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser(int id, User user)
         {
+            User dbUser = db.Users.Find(id);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            dbUser.Email = user.Email;
+            dbUser.Active = user.Active;
+            dbUser.Name = user.Name;
+            dbUser.Surname = user.Surname;
+            
+            //Sólo actualiza la contraseña si la proporciona
+            if (!string.IsNullOrWhiteSpace(user.Password))
+            {
+                dbUser.Password = this.EncryptPassword(user.Password);
+            }
+
+            db.Entry(dbUser).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/User
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            user.Password = this.EncryptPassword(user.Password);
+
+            db.Users.Add(user);
+
+            try 
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException dex)
+            {
+                return BadRequest(dex.InnerException.InnerException.Message);
+            }
+            
+
+            return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
         }
 
         // DELETE: api/User/5
-        public void Delete(int id)
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(int id)
         {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.Users.Count(e => e.Id == id) > 0;
+        }
+
+        private string EncryptPassword(string password)
+        {
+            SHA1 sha1 = SHA1.Create();
+
+            byte[] hashData = sha1.ComputeHash(Encoding.Default.GetBytes(password));
+
+            StringBuilder returnValue = new StringBuilder();
+
+            for (int i = 0; i < hashData.Length; i++)
+            {
+                returnValue.Append(hashData[i].ToString("x2"));
+            }
+
+            return returnValue.ToString();
         }
     }
 }
